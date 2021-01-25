19a6340c27983adaa088d24b917b8edaae9ffe4b
AwesomeCore1 <51826058+AwesomeCore1@users.noreply.github.com>
2021-01-25 20:51:54 +0100

    Should work with as updated script

--- __resource.lua
+++ __resource.lua
@@ -1,4 +1,4 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-client_script "cl_staff.lua"
-server_script "sv_staff.lua"
+client_script "client_staff.lua"
+server_script "server_staff.lua"
